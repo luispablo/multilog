1c17ebcfe7441ee74eedf75d5211351df01534b6
Luis Pablo <luispablo.gallo@gmail.com>
2019-12-09 12:26:49 -0300

    Replace tabs with spaces

--- test/ConsoleLogger-test.js
+++ test/ConsoleLogger-test.js
@@ -5,53 +5,53 @@ var ConsoleLogger = require("../lib/ConsoleLogger");
 var level = require("../lib/Level")();
 
 var console = {
-	lastMessage: "",
-	log: function (message) {
-		this.lastMessage = message;
-	}
+  lastMessage: "",
+  log: function (message) {
+    this.lastMessage = message;
+  }
 };
 var log = ConsoleLogger(console, level.DEBUG);
 var MESSAGE = "test message";
 
 test("ConsoleLogger - tell its level", function (assert) {
-	assert.equal(log.level, level.DEBUG, "It's level is DEBUG");
-	assert.end();
+  assert.equal(log.level, level.DEBUG, "It's level is DEBUG");
+  assert.end();
 });
 
 test("ConsoleLogger - uses the level", function (assert) {
-	var one = "1";
-	var levelLog = ConsoleLogger(console, "ERROR");
-	levelLog.warn(one);
-	assert.ok(console.lastMessage.indexOf(one) < 0, "Mustn't have logged yet");
-	levelLog = ConsoleLogger(console, "WARN");
-	levelLog.warn(one);
-	assert.ok(console.lastMessage.indexOf(one) >= 0, "Now it must have logged");
-	assert.end();
+  var one = "1";
+  var levelLog = ConsoleLogger(console, "ERROR");
+  levelLog.warn(one);
+  assert.ok(console.lastMessage.indexOf(one) < 0, "Mustn't have logged yet");
+  levelLog = ConsoleLogger(console, "WARN");
+  levelLog.warn(one);
+  assert.ok(console.lastMessage.indexOf(one) >= 0, "Now it must have logged");
+  assert.end();
 });
 
 test("ConsoleLogger - info works", function (assert) {
-	log.info(MESSAGE);
-	assert.equal(console.lastMessage, "INFO: "+ MESSAGE, "Formatted info level message");
-	assert.end();
+  log.info(MESSAGE);
+  assert.equal(console.lastMessage, "INFO: "+ MESSAGE, "Formatted info level message");
+  assert.end();
 });
 
 test("ConsoleLogger - warn works", function (assert) {
-	log.warn(MESSAGE);
-	assert.equal(console.lastMessage, "WARN: "+ MESSAGE, "Formatted warning message");
-	assert.end();
+  log.warn(MESSAGE);
+  assert.equal(console.lastMessage, "WARN: "+ MESSAGE, "Formatted warning message");
+  assert.end();
 });
 
 test("ConsoleLogger - debug works", function (assert) {
-	log.debug(MESSAGE);
-	assert.equal(console.lastMessage, "DEBUG: "+ MESSAGE, "Formatted debug message");
-	assert.end();
+  log.debug(MESSAGE);
+  assert.equal(console.lastMessage, "DEBUG: "+ MESSAGE, "Formatted debug message");
+  assert.end();
 });
 
 test("ConsoleLogger - error works", function (assert) {
-	log.error({code: 5, message: MESSAGE});
+  log.error({code: 5, message: MESSAGE});
 
-	var expectedMessage = "ERROR: {\"code\":5,\"message\":\""+ MESSAGE +"\"}";
+  var expectedMessage = "ERROR: {\"code\":5,\"message\":\""+ MESSAGE +"\"}";
 
-	assert.equal(console.lastMessage, expectedMessage, "Formatted error level message");
-	assert.end();
+  assert.equal(console.lastMessage, expectedMessage, "Formatted error level message");
+  assert.end();
 });
