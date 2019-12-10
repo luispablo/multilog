c7e2bbe06e186c8ef6f8a3ba0d4aae83279c1100
Luis Pablo <luispablo.gallo@gmail.com>
2019-12-10 11:16:32 -0300

    Changed linting tab rule from tabs to 2 spaces

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Builder = require("./lib/Builder");
 var MultiLogger = require("./lib/MultiLogger");
 
 module.exports = function (properties) {
-	var loggers = properties.map(function (props) { return Builder(props); });
+  var loggers = properties.map(function (props) { return Builder(props); });
 
-	return MultiLogger(loggers);
+  return MultiLogger(loggers);
 };
